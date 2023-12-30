0a9a9f6823c5b89ee86770a63f3c4af44ea1eb12
tgarmenliev <tihomir.garmenliev@gmail.com>
2023-12-30 13:30:52 +0200

    Make example for testing

---
+++ app/src/main/java/com/example/vlak_app_test/viewmodels/schedule/example_schedule.kt
@@ -0,0 +1,184 @@
+package com.example.vlak_app_test.viewmodels.schedule
+
+val sampleScheduleInfo = Schedule.ScheduleTable(
+    date = "2021-10-10",
+    route = "София - Пловдив",
+    options = listOf(
+        Schedule.Options(
+            duration = "1:30",
+            departureTime = "10:00",
+            arrivalTime = "11:30",
+            departureDate = "2021-10-10",
+            arrivalDate = "2021-10-10",
+            numOfTransfers = 0,
+            trains = listOf(
+                Schedule.Trains(
+                    from = "София",
+                    to = "Пловдив",
+                    depart = "10:00",
+                    arrive = "11:30",
+                    departDate = "2021-10-10",
+                    arriveDate = "2021-10-10",
+                    trainType = "ПВ",
+                    trainNum = "30121",
+                    duration = "1:30",
+                    timeToWaitNext = "0:00"
+                )
+            )
+        ),
+        Schedule.Options(
+            duration = "1:40",
+            departureTime = "12:00",
+            arrivalTime = "13:40",
+            departureDate = "2021-11-10",
+            arrivalDate = "2021-11-10",
+            numOfTransfers = 0,
+            trains = listOf(
+                Schedule.Trains(
+                    from = "София",
+                    to = "Пловдив",
+                    depart = "12:00",
+                    arrive = "13:40",
+                    departDate = "2021-11-10",
+                    arriveDate = "2021-11-10",
+                    trainType = "ПВ",
+                    trainNum = "30123",
+                    duration = "1:40",
+                    timeToWaitNext = "0:00"
+                )
+            )
+        ),
+        Schedule.Options(
+            duration = "1:40",
+            departureTime = "12:00",
+            arrivalTime = "13:40",
+            departureDate = "2021-11-10",
+            arrivalDate = "2021-11-10",
+            numOfTransfers = 0,
+            trains = listOf(
+                Schedule.Trains(
+                    from = "София",
+                    to = "Пловдив",
+                    depart = "12:00",
+                    arrive = "13:40",
+                    departDate = "2021-11-10",
+                    arriveDate = "2021-11-10",
+                    trainType = "ПВ",
+                    trainNum = "30123",
+                    duration = "1:40",
+                    timeToWaitNext = "0:00"
+                )
+            )
+        ),
+        Schedule.Options(
+            duration = "1:40",
+            departureTime = "12:00",
+            arrivalTime = "13:40",
+            departureDate = "2021-11-10",
+            arrivalDate = "2021-11-10",
+            numOfTransfers = 0,
+            trains = listOf(
+                Schedule.Trains(
+                    from = "София",
+                    to = "Пловдив",
+                    depart = "12:00",
+                    arrive = "13:40",
+                    departDate = "2021-11-10",
+                    arriveDate = "2021-11-10",
+                    trainType = "ПВ",
+                    trainNum = "30123",
+                    duration = "1:40",
+                    timeToWaitNext = "0:00"
+                )
+            )
+        ),
+        Schedule.Options(
+            duration = "1:40",
+            departureTime = "12:00",
+            arrivalTime = "13:40",
+            departureDate = "2021-11-10",
+            arrivalDate = "2021-11-10",
+            numOfTransfers = 0,
+            trains = listOf(
+                Schedule.Trains(
+                    from = "София",
+                    to = "Пловдив",
+                    depart = "12:00",
+                    arrive = "13:40",
+                    departDate = "2021-11-10",
+                    arriveDate = "2021-11-10",
+                    trainType = "ПВ",
+                    trainNum = "30123",
+                    duration = "1:40",
+                    timeToWaitNext = "0:00"
+                )
+            )
+        ),
+        Schedule.Options(
+            duration = "1:40",
+            departureTime = "12:00",
+            arrivalTime = "13:40",
+            departureDate = "2021-11-10",
+            arrivalDate = "2021-11-10",
+            numOfTransfers = 0,
+            trains = listOf(
+                Schedule.Trains(
+                    from = "София",
+                    to = "Пловдив",
+                    depart = "12:00",
+                    arrive = "13:40",
+                    departDate = "2021-11-10",
+                    arriveDate = "2021-11-10",
+                    trainType = "ПВ",
+                    trainNum = "30123",
+                    duration = "1:40",
+                    timeToWaitNext = "0:00"
+                )
+            )
+        ),
+        Schedule.Options(
+            duration = "1:40",
+            departureTime = "12:00",
+            arrivalTime = "13:40",
+            departureDate = "2021-11-10",
+            arrivalDate = "2021-11-10",
+            numOfTransfers = 0,
+            trains = listOf(
+                Schedule.Trains(
+                    from = "София",
+                    to = "Пловдив",
+                    depart = "12:00",
+                    arrive = "13:40",
+                    departDate = "2021-11-10",
+                    arriveDate = "2021-11-10",
+                    trainType = "ПВ",
+                    trainNum = "30123",
+                    duration = "1:40",
+                    timeToWaitNext = "0:00"
+                )
+            )
+        ),
+        Schedule.Options(
+            duration = "1:40",
+            departureTime = "12:00",
+            arrivalTime = "13:40",
+            departureDate = "2021-11-10",
+            arrivalDate = "2021-11-10",
+            numOfTransfers = 0,
+            trains = listOf(
+                Schedule.Trains(
+                    from = "София",
+                    to = "Пловдив",
+                    depart = "12:00",
+                    arrive = "13:40",
+                    departDate = "2021-11-10",
+                    arriveDate = "2021-11-10",
+                    trainType = "ПВ",
+                    trainNum = "30123",
+                    duration = "1:40",
+                    timeToWaitNext = "0:00"
+                )
+            )
+        ),
+    )
+)
