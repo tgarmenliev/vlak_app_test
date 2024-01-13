e9e8fd74e3a27162d316d93d16cd076f5afd0fdc
tgarmenliev <tihomir.garmenliev@gmail.com>
2024-01-13 22:53:23 +0200

    Make schedule view model

--- app/src/main/java/com/example/vlak_app_test/ui/schedule/ScheduleViewModel.kt
+++ app/src/main/java/com/example/vlak_app_test/ui/schedule/ScheduleViewModel.kt
@@ -1,2 +1,28 @@
 package com.example.vlak_app_test.ui.schedule
 
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import com.example.vlak_app_test.viewmodels.schedule.Schedule
+import com.example.vlak_app_test.viewmodels.schedule.sampleScheduleInfo
+
+class ScheduleViewModel : ViewModel() {
+    private val _selectedStations = mutableStateOf(ScheduleOption("", "", ""))
+    val selectedStations: State<ScheduleOption> = _selectedStations
+
+    private val data: Schedule.ScheduleTable = sampleScheduleInfo
+
+    fun setOption(from: String, to: String, date: String) {
+        _selectedStations.value = ScheduleOption(from, to, date)
+    }
+
+    fun getData(): Schedule.ScheduleTable {
+        return data
+    }
+}
+
+data class ScheduleOption(
+    val from: String,
+    val to: String,
+    val date: String,
+)
