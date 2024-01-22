8162adc48f149a46ecbfc487c168aa434040f63f
tisho <tihomir.garmenliev@gmail.com>
2024-01-22 10:14:45 +0200

    Add to UI

--- app/src/main/java/com/example/vlak_app_test/ui/schedule/ScheduleScreenSearch.kt
+++ app/src/main/java/com/example/vlak_app_test/ui/schedule/ScheduleScreenSearch.kt
@@ -24,10 +24,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.vlak_app_test.R
