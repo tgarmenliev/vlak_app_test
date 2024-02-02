a35d0e2e006bf1175030693ce32d0de898625969
tgarmenliev <tihomir.garmenliev@gmail.com>
2024-02-02 16:46:09 +0200

    Get guide from server

--- app/src/main/java/com/example/vlak_app_test/ui/guide/GuideMoreInfoScreen.kt
+++ app/src/main/java/com/example/vlak_app_test/ui/guide/GuideMoreInfoScreen.kt
@@ -22,9 +22,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.example.vlak_app_test.R
 
 @Composable
@@ -59,9 +62,11 @@ fun MakeGuideMoreInfoScreen(
                 .clickable { onClose() },
         )
 
-        Image(
-            painter = painterResource(id = R.drawable.train_one),
-            contentDescription = "Guide image",
+        AsyncImage(
+            model = ImageRequest.Builder(LocalContext.current)
+                .data(url + "/guide/images/" + data.image)
+                .build(),
+            contentDescription = "topic image",
             contentScale = ContentScale.Crop,
             modifier = Modifier
                 .fillMaxWidth(0.9f)
@@ -70,6 +75,18 @@ fun MakeGuideMoreInfoScreen(
                 .align(Alignment.CenterHorizontally)
                 .clip(RoundedCornerShape(20.dp))
         )
+
+//        Image(
+//            painter = painterResource(id = R.drawable.train_one),
+//            contentDescription = "Guide image",
+//            contentScale = ContentScale.Crop,
+//            modifier = Modifier
+//                .fillMaxWidth(0.9f)
+//                .height(200.dp)
+//                .padding(top = 16.dp, bottom = 16.dp)
+//                .align(Alignment.CenterHorizontally)
+//                .clip(RoundedCornerShape(20.dp))
+//        )
 
         Column(
             modifier = Modifier
@@ -95,6 +112,19 @@ fun MakeGuideMoreInfoScreen(
                     style = MaterialTheme.typography.bodyMedium,
                     modifier = Modifier.padding(bottom = 8.dp)
                 )
+
+                if (it.image.isNotEmpty()) {
+                    AsyncImage(
+                        model = ImageRequest.Builder(LocalContext.current)
+                            .data(url + "/guide/images/" + it.image)
+                            .build(),
+                        contentDescription = "topic image",
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier
+                            .padding(bottom = 16.dp)
+                            .clip(RoundedCornerShape(20.dp))
+                    )
+                }
             }
         }
     }
