62dc3b5fe22df26047a49524f02174c0d8ff48de
tgarmenliev <tihomir.garmenliev@gmail.com>
2024-02-10 14:06:35 +0200

    remove unnecessary big comments

--- app/src/main/java/com/example/vlak_app_test/datastore/Datastore.kt
+++ app/src/main/java/com/example/vlak_app_test/datastore/Datastore.kt
@@ -30,22 +30,3 @@ class DataStoreManager(context: Context) {
         val DARK_MODE_KEY = booleanPreferencesKey("dark_mode")
     }
 }
-
-//val context = LocalContext.current
-//// Define a DataStore for user preferences
-//val dataStore: DataStore<Preferences> = context.createDataStore(name = "user_preferences")
-//
-//// Define a key for the dark mode
-//val DARK_MODE_KEY = booleanPreferencesKey("dark_mode")
-//
-//// Function to store the toggle value
-//suspend fun storeDarkMode(isOn: Boolean) {
-//    dataStore.edit { preferences ->
-//        preferences[DARK_MODE_KEY] = isOn
-//    }
-//}
-//
-//// Function to retrieve the dark mode value
-//val darkModeFlow: Flow<Boolean?> = dataStore.data.map { preferences ->
-//    preferences[DARK_MODE_KEY]
-//}
