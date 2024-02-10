8dc3701cb4fddb96fec0cea9df67fffd59a5a46c
tgarmenliev <tihomir.garmenliev@gmail.com>
2024-02-10 14:19:16 +0200

    remove unnecessary imports

--- app/src/main/java/com/example/vlak_app_test/ui/schedule/ScheduleScreenSearch.kt
+++ app/src/main/java/com/example/vlak_app_test/ui/schedule/ScheduleScreenSearch.kt
@@ -36,7 +36,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
