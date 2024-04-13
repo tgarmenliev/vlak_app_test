6b31ff852ea551fbbc6c393b42b87fd12090126e
Tihomir Garmenliev <101339875+tgarmenliev@users.noreply.github.com>
2024-04-13 14:49:42 +0300

    add new version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.bultrain.vlak_app_test"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
