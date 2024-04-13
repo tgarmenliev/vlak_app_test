29852d6585539143852cd73068b0dc8d407c869e
Tihomir Garmenliev <101339875+tgarmenliev@users.noreply.github.com>
2024-04-13 15:45:47 +0300

    add settings button

--- app/src/main/java/com/bultrain/vlak_app_test/ui/composables/MakeImageHeader.kt
+++ app/src/main/java/com/bultrain/vlak_app_test/ui/composables/MakeImageHeader.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -42,6 +43,7 @@ fun MakeImageHeader(
                     .align(Alignment.TopStart)
                     .padding(start = 8.dp)
                     .padding(top = 8.dp)
+                    .size(36.dp)
                     .clickable { onButtonClicked() }
             )
         }
