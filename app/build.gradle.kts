a59b2c95f8d04ad5e6b340a65ac76e2c4e72d290
Tihomir Garmenliev <101339875+tgarmenliev@users.noreply.github.com>
2024-06-27 22:44:14 +0300

    [FIX] change versions of the app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.bultrain.vlak_app_test"
         minSdk = 24
         targetSdk = 34
-        versionCode = 4
-        versionName = "1.2.1"
+        versionCode = 6
+        versionName = "1.2.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
